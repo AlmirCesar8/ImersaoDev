0df655a04369ecc46e5449e73d4d36e7f2643254
Almircesar8 <cesar25silvafilho@gmail.com>
2024-09-07 18:17:10 -0300

    aula5

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 function pesquisar() {
-    // Obtém a seção onde os resultados serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
+  // Obtém a seção onde os resultados serão exibidos
+  let section = document.getElementById("resultados-pesquisa");
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
 
-    // Inicializa uma string vazia para armazenar os resultados
-    let resultado = "";
+  // Inicializa uma string vazia para armazenar os resultados
+  let resultado = "";
+  let nome = "";
+  let posicao = "";
 
-    // Itera sobre cada dado na lista de dados
-    for (let dado of dados) {
-        // Cria um novo elemento div para cada resultado
-        resultado += `
-        <div class="item-resultado">
-          <h2><a href=${dado.insta} target="_blank">${dado.nome}</a></h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <a href=${dado.wiki} target="_blank" class="mais-informacoes">Mais informações</a>
-        </div>
+  // Itera sobre cada dado na lista de dados
+  for (let dado of dados) {
+
+    nome = dado.nome.toLowerCase()
+    posicao = dado.posicao.toLowerCase()
+
+    if (nome.includes(campoPesquisa) || posicao.includes(campoPesquisa)) {
+      // Cria um novo elemento div para cada resultado
+      resultado += `
+      <div class="item-resultado">
+        <h2><a href=${dado.insta} target="_blank">${dado.nome}</a></h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <a href=${dado.wiki} target="_blank" class="mais-informacoes">Mais informações</a>
+      </div>
       `;
     }
-
-    // Atribui o HTML gerado à seção de resultados
-    section.innerHTML = resultado;
+  }
+  if (!resultado) {
+    resultado = '<div class= "item-resultado"><h2>Nada foi encontrado</h2></div>'
+  }
+  // Atribui o HTML gerado à seção de resultados
+  section.innerHTML = resultado;
 }
