7eea03f6f50a0a55b341bcb62778fb7e5a3adc3b
Almircesar8 <cesar25silvafilho@gmail.com>
2024-09-08 01:24:20 -0300

    add_jogadores

--- app.js
+++ app.js
@@ -10,13 +10,24 @@ function pesquisar() {
 
     if (dado.nome.toLowerCase().includes(campoPesquisa.toLowerCase()) || dado.posicao.toLowerCase().includes(campoPesquisa.toLowerCase()) || dado.nacionalidade.toLowerCase().includes(campoPesquisa.toLowerCase())) {
       // Cria um novo elemento div para cada resultado
-      resultado += `
-      <div class="item-resultado">
-        <h2><a href=${dado.insta} target="_blank">${dado.nome}</a></h2>
-        <p class="descricao-meta">${dado.descricao}</p>
-        <a href=${dado.wiki} target="_blank" class="mais-informacoes">Mais informações</a>
-      </div>
-      `;
+      if (dado.posicao != "goleiro"){
+        resultado += `
+        <div class="item-resultado">
+          <h2><a href=${dado.insta} target="_blank">${dado.nome}</a></h2>
+          <p>Naturalizado(a) ${dado.nacionalidade} e com ${dado.idade} anos, o camisa ${dado.camisa} do Corinthians é um(a) ${dado.posicao} que já disputou ${dado.jogos} jogos, marcando ${dado.gols} gols e distribuindo ${dado.assistencias} assistências. ${dado.descricao}</p>
+          <a href="${dado.wiki}" target="_blank">Mais informações</a>
+        </div>
+        `;
+      }
+      else{
+        resultado += `
+        <div class="item-resultado">
+          <h2><a href=${dado.insta} target="_blank">${dado.nome}</a></h2>
+          <p>Naturalizado(a) ${dado.nacionalidade} e com ${dado.idade} anos, o camisa ${dado.camisa} do Corinthians é um(a) ${dado.posicao} que já disputou ${dado.jogos} jogos, defendendo ${dado.penal_def} pênaltis e sofrendo ${dado.gols_sofri} gols. ${dado.descricao}</p>
+          <a href="${dado.wiki}" target="_blank">Mais informações</a>
+        </div>
+        `;
+      }
     }
   }
   if (!resultado) {
